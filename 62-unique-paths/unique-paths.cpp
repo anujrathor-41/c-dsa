2285fda35ac0d5c3f351fb37023a9ff1ceec0b52
Anuj Rathor <anujrathor41@gmail.com>
2026-07-03 01:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (86.40%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -37,25 +37,42 @@
 // };
 
 
-class Solution {// bottom upand  use karte hai loop and usi function ko complete karte hai
+// class Solution {// bottom upand  use karte hai loop and usi function ko complete karte hai
+// public:
+
+
+
+//     int uniquePaths(int m, int n) {//without using memset or sir ka tarika
+//        vector<vector<int>> dp(m,vector<int> (n,-1));// placing -1 in grid
+//         for(int i=0;i<m;i++){
+//             dp[i][n-1] =1;// 1 dal diya col last col main
+//         }
+//         for(int j=0;j<n;j++){
+//             dp[m-1][j]=1;
+//         }
+//         for(int i=m-2;i>=0;i--){
+//             for(int j=n-2;j>=0;j--){
+//                 dp[i][j] = dp[i][j+1] + dp[i+1][j];
+//             }
+//         }
+//         return dp[0][0]; // ans kaha hoga 
+        
+//     }
+// };
+class Solution {// space optimization and using only 1d arr 
 public:
 
+    int uniquePaths(int m, int n) {
+       vector<int> dp(n,1);// declare 1d dp
+       for(int i=0;i<m-1;i++){
+        for(int j=n-2;j>=0;j--){
+            dp[j]=dp[j] + dp[j+1];
+        }
+       }
+       return dp[0]; // kyu 1d arr hai aur hamara ans 0index ki dp main hoga
 
 
-    int uniquePaths(int m, int n) {//without using memset or sir ka tarika
-       vector<vector<int>> dp(m,vector<int> (n,-1));// placing -1 in grid
-        for(int i=0;i<m;i++){
-            dp[i][n-1] =1;// 1 dal diya col last col main
-        }
-        for(int j=0;j<n;j++){
-            dp[m-1][j]=1;
-        }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j] = dp[i][j+1] + dp[i+1][j];
-            }
-        }
-        return dp[0][0]; // ans kaha hoga 
+        
         
     }
 };
